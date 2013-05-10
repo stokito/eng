b1fac59243d187a5fbee61edd1cc21c649701cbd
Sergey Ponomarev <stokito@gmail.com>
2013-05-10 15:26:45 +0300

    init

---
+++ src/main/java/eng/PartOfLang.java
@@ -0,0 +1,40 @@
+package eng;
+
+public enum PartOfLang {
+	/**
+	 * глаголы
+	 */
+	VERBS,
+	/**
+	 * существительные
+	 */
+	NOUNS,
+	/**
+	 * артикли
+	 */
+	ARTICLES,
+	/**
+	 * прилагательные
+	 */
+	ADJECTIVES,
+	/**
+	 * местоимения
+	 */
+	PRONOUNS,
+	/**
+	 * числительные
+	 */
+	NUMERALS,
+	/**
+	 * наречия
+	 */
+	ADVERBS,
+	/**
+	 * предлоги
+	 */
+	PREPOSITIONS,
+	/**
+	 * союзы
+	 */
+	CONJUNCTIONS
+}
